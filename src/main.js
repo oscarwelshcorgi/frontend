e58726fe9a427926e42c8c8ee1bd0d9da1ad5faa
GIGEON.MUN <ggmun@lfit.co.kr>
2023-07-06 17:42:26 +0900

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import {createRouter, createWebHistory} from 'vue-router'
+import Home from '@/pages/Home';
+import Login from '@/pages/Login';
+
+const routes = [
+    {path:'/', component: Home},
+    {path:'/login', component: Login}
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+createApp(App).use(router).mount('#app')
