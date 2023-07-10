b859ac271783e426b957331d52e6b205056441fa
GIGEON.MUN <ggmun@lfit.co.kr>
2023-07-10 18:23:42 +0900

    2

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,30 @@
 <script>
 import Footer from '@/components/Footer.vue'
 import Header from '@/components/Header.vue'
+import store from '@/scripts/store'
+import axios from 'axios'
+import {watch} from "vue";
+import {useRoute} from "vue-router/dist/vue-router";
 
 export default {
   name: 'App',
   components: {
     Footer,
     Header
+  },
+  setup() {
+    const check = () => {
+        axios.get("/api/account/check").then(({data}) => {
+            console.log(data);
+                store.commit("setAccount", data || 0);
+        })
+    };
+
+    const route = useRoute();
+
+    watch(route, () => {
+        check();
+    })
   }
 }
 </script>
